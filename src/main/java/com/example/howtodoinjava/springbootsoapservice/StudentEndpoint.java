b1f34bdf633c12e8d8f01a35a9764481b17decb8
Faizal Patel <faizalmw@hotmail.com>
2020-04-03 15:34:08 +0100

    Add sprintboot soap example

---
+++ src/main/java/com/example/howtodoinjava/springbootsoapservice/StudentEndpoint.java
@@ -0,0 +1,31 @@
+package com.example.howtodoinjava.springbootsoapservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import com.howtodoinjava.xml.school.StudentDetailsRequest;
+import com.howtodoinjava.xml.school.StudentDetailsResponse;
+
+@Endpoint
+public class StudentEndpoint {
+	private static final String NAMESPACE_URI = "http://www.howtodoinjava.com/xml/school";
+
+	private StudentRepository StudentRepository;
+
+	@Autowired
+	public StudentEndpoint(StudentRepository StudentRepository) {
+		this.StudentRepository = StudentRepository;
+	}
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "StudentDetailsRequest")
+	@ResponsePayload
+	public StudentDetailsResponse getStudent(@RequestPayload StudentDetailsRequest request) {
+		StudentDetailsResponse response = new StudentDetailsResponse();
+		response.setStudent(StudentRepository.findStudent(request.getName()));
+
+		return response;
+	}
+}
